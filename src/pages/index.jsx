0e2c039de049675db2e7c576157897cc9f1e740f
evan-johns <evandjohns@gmail.com>
2023-02-08 23:44:59 -0500

    text updates landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -261,10 +261,10 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Software engineer, enriching people's lives through technology.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Evan, a software designer and entrepreneur based in Toledo, Ohio. I’m the founder and CEO of Planetaria, where we develop
+            I’m Evan, a software engineer and freelance web developer based in Toledo, Ohio. I’m the founder and CEO of Planetaria, where we develop
             technologies that empower regular people to explore space on their
             own terms.
           </p>
